423b1c053ccc0c1090d43bc0b3de80f7e128beab
Jrevoredo42 <revoredojf@gmail.com>
2022-01-18 14:11:28 -0300

    Foi criado projeto de uma aplicaçao de contador utilizando javascript, com sua estilizaçao e dimensionamento basico

---
+++ baseCampJavascript/Introducao a Javascript/contador/assets/scripts.js
@@ -0,0 +1,12 @@
+var currentNumber = 0;
+var currentNumberWrapper = document.getElementById("currentNumber");
+
+function increment(){
+    currentNumber++;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement(){
+    currentNumber--;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
