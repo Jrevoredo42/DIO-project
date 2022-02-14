cab074ba0c4154bc61875c7231eee8a5e9f5b316
Jrevoredo42 <revoredojf@gmail.com>
2022-02-14 20:01:17 -0300

    foi criado diretorio para armazenamento dos codigos, cursos e projeto referentes ao basecamp de reactJS

---
+++ baseCampReact/Introdução a React/webpack-basico/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <div>
+            <p>DIO</p>
+            <p>Projeto react com webpack</p>
+        </div>
+    )
+}
+export default App;
